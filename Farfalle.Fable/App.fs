d2f8beaacb43c47638607308b83991dfc81abd81
Gábor Gergely <ggergely@kodfodrasz.net>
2021-03-22 18:25:16 +0100

    Update Fable and adapt to fixes Document API

--- Farfalle.Fable/App.fs
+++ Farfalle.Fable/App.fs
@@ -4,21 +4,8 @@ open Browser.Dom
 open Browser.Types
 
 
-// Mutable variable to count the number of times we clicked the button
-let mutable count = 0
-
-// Get a reference to our button and cast the Element to an HTMLButtonElement
-let myButton =
-  document.querySelector ("#my-button") :?> Browser.Types.HTMLButtonElement
-
-// Register our listener
-myButton.onclick <-
-  fun _ ->
-    count <- count + 1
-    myButton.innerText <- sprintf "You clicked: %i time(s)" count
-
 // Paste event
-document.body.onpaste <-
+document.onpaste <-
   fun e ->
     let logger msg = console.log (sprintf "paste %s" msg)
     logger "event received"
